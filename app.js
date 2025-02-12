a09874ae0ff24f4441a3db553a7da2a1ff43e9de
nmalle1 <nikhilreddy.malle@slu.edu>
2025-02-12 05:33:39 +0000

    lab4 submission

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const express = require('express')
 
 // Set the port
+// Set port and this is 3000 port
 const port = process.env.PORT || 3000
 // Boot the app
 const app = express()
@@ -12,13 +13,11 @@ app.use(express.static(__dirname + '/public'));
 app.get('/products', listProducts)
 app.get('/', handleRoot);
 // Boot the server
+// Boot server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
 
 /**
- * Handle the root route
- * @param {object} req
- * @param {object} res
-*/
+@@ -22,7 +23,6 @@ app.listen(port, () => console.log(`Server listening on port ${port}`))
 function handleRoot(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 }
